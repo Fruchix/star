e94b5001b61650d97d8cf3d58a8229996d82e351
Fruchix <fruchix@gmail.com>
2026-06-17 22:00:17 +0200

    misc: build one docker image per branch
    This allows multiple worktree development, without overwriting the same image in docker.

--- docs/docker-devel/build.sh
+++ docs/docker-devel/build.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-docker build -t star-ubuntu -f docs/docker-devel/Dockerfile .
+
+git_branch="$(git branch --show-current)"
+
+if [[ -z "$git_branch" ]]; then
+  echo "Error: could not determine git branch." >&2
+  exit 1
+fi
+
+clean_git_branch=$(echo "$git_branch" | tr --complement --delete '[:alnum:]' | tr '[:upper:]' '[:lower:]')
+
+echo "Building Docker image for branch '$git_branch'."
+
+docker build -t "star-ubuntu-${clean_git_branch}" -f docs/docker-devel/Dockerfile .
--- docs/docker-devel/run.sh
+++ docs/docker-devel/run.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-docker run --rm -it star-ubuntu
+
+git_branch="$(git branch --show-current)"
+
+if [[ -z "$git_branch" ]]; then
+  echo "Error: could not determine git branch." >&2
+  exit 1
+fi
+
+clean_git_branch=$(echo "$git_branch" | tr --complement --delete '[:alnum:]' | tr '[:upper:]' '[:lower:]')
+
+echo "Running Docker image for branch '$git_branch'."
+
+docker run --rm -it "star-ubuntu-${clean_git_branch}"
